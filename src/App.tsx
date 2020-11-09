08df728ba74c48c163be1add9621977e218e3f81
Jefferson(Zhe Fan) Li <jeffersonli.li@mail.utoronto.ca>
2020-11-09 13:48:26 -0500

    add desktop encouragement

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import Parent from './components/Parent/Parent';
-function App() {
-  return (
-    <div className="App">
-      <Parent></Parent>
-    </div>
-  );
-}
+export default class App extends React.Component<any, any>{
 
-export default App;
+  constructor(props: any){
+    super(props)
+
+    let replacementComp = (a: boolean) => a ? <div id="warning"> Best Viewed on Desktop !</div> : <></>
+    this.state = { mobile: replacementComp(window.matchMedia("(max-width: 800px)").matches)};
+    let handleEvent = (e: any)=>{
+      this.setState({...App, mobile : replacementComp(e.matches)})
+    }
+    window.matchMedia("(max-width: 800px)").addEventListener("change", handleEvent);
+  }
+
+  render(){
+     return(
+      <div className="App">
+        {this.state.mobile}
+        <Parent></Parent>
+     </div>
+     )
+  }
+}
