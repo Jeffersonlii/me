0f900111522ecc4f2b3b31696c83ca2545588383
Jefferson(Zhe Fan) Li <jeffersonli.li@mail.utoronto.ca>
2020-11-12 02:36:18 -0500

    finish visuals of dropdown

--- src/components/MainOS/MainOS.tsx
+++ src/components/MainOS/MainOS.tsx
@@ -1,69 +1,29 @@
 import React, { useState } from 'react';
 import Draggable from '../Draggable/CustomDraggable';
-import logo from './../../public/appLogo.png';
-import { BootSequence, BootText } from './BootSequence';
+import { BootSequence } from './BootSequence';
 import './BootSequence.scss';
-import Dropdown from './Dropdown/Dropdown';
 import './MainOS.scss';
+import OSHeader from './OSHeader/OSHeader';
 import DesktopFiles from './Pages/Pages';
 
 export default function MainOS (props: any) {
-    let dropDownStatics = [{
-                label: 'File',
-                options: [{
-                    option: 'lol xd'
-                    }]
-                },
-                {
-                    label: 'Edit',
-                    options: [{
-                        option: 'lol xd'
-                    }]
-                },
-                {
-                    label: 'View',
-                    options: [{
-                        option: 'lol xd'
-                    }]
-                },
-                {
-                    label: 'Special',
-                    options: [{
-                        option: 'lol xd'
-                    }]
-                }
-            ]
-    let dropdowns = dropDownStatics.map((el, index)=>{
-            return {...el, id: index, options: 
-                el.options.map((op, index)=>({...op, id: index})
-            )}
-        })
-    
 
     const onClickBoot = () =>{
         setBodyComp(
         <>
-            <div className="fadein">
+            {/* <div className="fadein">
                 <div className="initText">
                     <BootText></BootText>
                 </div>
-            </div>
+            </div> */}
             <div className="OSheader">
-                <div className="link img">
-                    <div className="img">
-                        <img src={logo} alt="apple logo" />
-                    </div>
-                </div>
-                <Dropdown labels={dropdowns}
-                ></Dropdown>
+                <OSHeader></OSHeader>
                 <div style={{ flexGrow: 9 }}></div>
             </div>
             <div id="desktop">
                 <DesktopFiles openWindow={props.openWindow}></DesktopFiles>
             </div>
-        </>
-        
-        )
+        </>)
         props.toggleAudio();
     }
     const [bodyComp, setBodyComp] = useState(
---
+++ src/components/MainOS/OSHeader/OSHeader.tsx
@@ -0,0 +1,124 @@
+import React, { useState } from 'react';
+import '../MainOS.scss';
+import logo from './../../../public/appLogo.png';
+import './OSHeader.scss';
+
+export interface DropDownLabel { 
+    label: string,
+    id: number,
+    options: {
+        option: string,
+        id: number,
+        disabled?: boolean
+        }[]
+}
+
+export default function OSHeader(props: any){
+    let dropDownStatics = [{
+        label: 'File',
+        options: [{
+                        option: 'New',
+                        disabled: true
+                    },
+                    {
+                        option: 'Open',
+                        disabled: true
+                    },
+                    {
+                        option: 'Get Info'
+                    },
+                    {
+                        option: 'Close'
+                    },
+                ]
+        },
+        {
+            label: 'Edit',
+            options: [{
+                option: 'Cut',
+                disabled: true
+            },
+            {
+                option: 'Copy',
+                disabled: true
+            },
+            {
+                option: 'Paste',
+                disabled: true
+            },
+            {
+                option: 'Clear',
+                disabled: true
+            },
+            {
+                option: 'Select All',
+                disabled: true
+            }]
+        },
+        {
+            label: 'View',
+            options: [{
+                option: 'Toggle Audio'
+            },
+            {
+                option: 'Toggle Audio'
+            },]
+        },
+        {
+            label: 'Special',
+            options: [{
+                option: 'Clean Up'
+            },
+            {
+                option: 'Erase Disk'
+            },
+            {
+                option: 'Show Hidden Folders'
+            }]
+        }
+    ]
+    let dropdowns: DropDownLabel[] = dropDownStatics.map((el, index)=>{
+        return {...el, id: index, options: 
+            el.options.map((op, index)=>({...op, id: index, disabled: op.disabled ?? false})
+        )}
+    })
+
+    const [group, setGroup] = useState(dropdowns.map((a)=>({...a, opened: false})))
+
+    let onToggle = (id: number, action: "enter" | "leave")=>{
+        setGroup(group.map((a)=>{
+            if(a.id === id) return {...a, opened: action === "enter"};
+            return a
+        }));
+    } 
+    return (
+        <>
+        <div className="link img">
+                <div className="img">
+                <img src={logo} alt="apple logo" />
+            </div>
+        </div>
+        {group.map((label) => {
+        return <div className="link" key={label.id} 
+                onMouseOver={(e)=>onToggle(label.id, "enter")}
+                onMouseLeave={(e)=>onToggle(label.id, "leave")} >
+                    {label.label}
+                    {(()=>{
+                        if(label.opened) 
+                            return <DropPanel label={label} ></DropPanel>
+                    })()}
+                </div>})}
+        </>)
+}
+
+function DropPanel(props: { label: DropDownLabel}){  
+    return <div className="panel">
+                {props.label.options.map((op) => { 
+
+                   return (op.disabled ?
+                     <div className="option disabled" key={op.id}>{op.option}</div>
+                     :
+                     <div className="option" key={op.id}>{op.option}</div>)
+                })}
+            </div>
+}
