5011050387545dabc72d1fd8030caf56a01383de
Jefferson(Zhe Fan) Li <jeffersonli.li@mail.utoronto.ca>
2021-01-04 18:25:33 -0500

    fix build

--- src/components/MainOS/MainOS.tsx
+++ src/components/MainOS/MainOS.tsx
@@ -1,43 +1,41 @@
 import React, { useState } from 'react';
 import Draggable from '../Draggable/CustomDraggable';
-import { BootSequence, BootText } from './BootSequence';
+import { BootSequence } from './BootSequence';
 import './BootSequence.scss';
 import './MainOS.scss';
 import OSHeader from './OSHeader/OSHeader';
 import DesktopFiles from './Pages/Pages';
 
-export default function MainOS (props: any) {
-
-    const onClickBoot = () =>{
-        setBodyComp(
-        <>
-            {/* <div className="fadein">
+export default function MainOS(props: any) {
+  const onClickBoot = () => {
+    setBodyComp(
+      <>
+        {/* <div className="fadein">
                 <div className="initText">
                     <BootText></BootText>
                 </div>
             </div> */}
-            <div className="OSheader">
-                <OSHeader></OSHeader>
-                <div style={{ flexGrow: 9 }}></div>
-            </div>
-            <div id="desktop">
-                <DesktopFiles openWindow={props.openWindow}></DesktopFiles>
-            </div>
-        </>)
-        props.toggleAudio();
-    }
-    const [bodyComp, setBodyComp] = useState(
-        <BootSequence onClickBoot={onClickBoot}></BootSequence>
+        <div className="OSheader">
+          <OSHeader></OSHeader>
+          <div style={{ flexGrow: 9 }}></div>
+        </div>
+        <div id="desktop">
+          <DesktopFiles openWindow={props.openWindow}></DesktopFiles>
+        </div>
+      </>
     );
-    return (
-    <Draggable 
-    handle=".OSheader"
-    defaultClassName="mainOS react-draggable"
-    focusable={false}
+    props.toggleAudio();
+  };
+  const [bodyComp, setBodyComp] = useState(
+    <BootSequence onClickBoot={onClickBoot}></BootSequence>
+  );
+  return (
+    <Draggable
+      handle=".OSheader"
+      defaultClassName="mainOS react-draggable"
+      focusable={false}
     >
-        <div className="OSbackground">
-            {bodyComp}
-        </div>  
-    </Draggable>);   
+      <div className="OSbackground">{bodyComp}</div>
+    </Draggable>
+  );
 }
-
