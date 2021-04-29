e9778c8a5c07decc0180dd1658c5ab8ed3d23856
Jefferson(Zhe Fan) Li <jeffersonli.li@mail.utoronto.ca>
2021-04-28 20:30:34 -0400

    2021 winter

--- src/components/MainOS/Pages/ExperiencePage.tsx
+++ src/components/MainOS/Pages/ExperiencePage.tsx
@@ -1,114 +1,126 @@
-import React from "react";
-import json from "../../../public/strings";
-import "./ExperiencePage.scss";
-import { $openWindow } from "./../../Parent/ParentService";
+import React from 'react';
+import json from '../../../public/strings';
+import './ExperiencePage.scss';
+import { $openWindow } from './../../Parent/ParentService';
 export default function ExperiencePage(props: any) {
-  const openExperience = (experienceName: string, experienceInfo: any) => {
-    $openWindow.next({
-      child: (
-        <ExperienceDetail
-          name={experienceName}
-          info={experienceInfo}
-        ></ExperienceDetail>
-      ),
-      params: {
-        header: `${experienceName.replace(/ /g, "_")}.HTML`,
-        width: "60rem",
-        height: "30rem",
-      },
-    });
-  };
+    const openExperience = (experienceName: string, experienceInfo: any) => {
+        $openWindow.next({
+            child: (
+                <ExperienceDetail
+                    name={experienceName}
+                    info={experienceInfo}
+                ></ExperienceDetail>
+            ),
+            params: {
+                header: `${experienceName.replace(/ /g, '_')}.HTML`,
+                width: '60rem',
+                height: '30rem',
+            },
+        });
+    };
 
-  const experienceTypes: { key: "work exp" | "projects"; name: string }[] = [
-    { key: "work exp", name: "Work Experience" },
-    { key: "projects", name: "Personal Projects" },
-  ];
-  return (
-    <>
-      {experienceTypes.map(({ key, name }) => {
-        return (
-          <>
-            {name}
-            <ul>
-              {Object.entries(json[key]).map(([name, info]) => {
+    const experienceTypes: { key: 'work exp' | 'projects'; name: string }[] = [
+        { key: 'work exp', name: 'Work Experience' },
+        { key: 'projects', name: 'Personal Projects' },
+    ];
+    return (
+        <>
+            {experienceTypes.map(({ key, name }) => {
                 return (
-                  <li
-                    onClick={() => {
-                      openExperience(name, info);
-                    }}
-                  >
-                    <div id="link">
-                      {name} : {info.time}
-                    </div>
-                  </li>
+                    <>
+                        {name}
+                        <ul>
+                            {Object.entries(json[key]).map(([name, info]) => {
+                                return (
+                                    <li
+                                        onClick={() => {
+                                            openExperience(name, info);
+                                        }}
+                                    >
+                                        <div id="link">
+                                            {name} : {info.time}
+                                        </div>
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    </>
                 );
-              })}
-            </ul>
-          </>
-        );
-      })}
-    </>
-  );
+            })}
+        </>
+    );
 }
 
 function ExperienceDetail(props: { name: string; info: any }) {
-  const openImage = (imgDetail: { url: string; desc: string }) => {
-    $openWindow.next({
-      child: (
-        <img
-          src={process.env.PUBLIC_URL + imgDetail.url}
-          id="expanded-img"
-          alt=""
-        />
-      ),
-      params: {
-        header: (imgDetail.url.split("/").pop() as string).toUpperCase(),
-        width: "30rem",
-        height: "100%",
-      },
-    });
-  };
-
-  return (
-    <>
-      <h1>
-        {props.name} - {props.info.time}
-      </h1>
-      <section>{props.info.desc}</section>
-      {Object.keys(props.info["modal_details"]).map((section) => {
-        return (
-          <ul>
-            <li>
-              {section}
-              <ul>
-                {props.info["modal_details"][section].map((point: string) => (
-                  <li dangerouslySetInnerHTML={{ __html: point }} />
-                ))}
-              </ul>
-            </li>
-          </ul>
-        );
-      })}
-      <section>Relevant Images</section>
-      <div className="img-grid">
-        {props.info["images"].map((img: { url: string; desc: string }) => {
-          return (
-            <div>
-              <ul>
+    const openImage = (imgDetail: { url: string; desc: string }) => {
+        $openWindow.next({
+            child: (
                 <img
-                  src={process.env.PUBLIC_URL + img.url}
-                  alt=""
-                  className="img-btn"
-                  onClick={() => {
-                    openImage(img);
-                  }}
+                    src={process.env.PUBLIC_URL + imgDetail.url}
+                    id="expanded-img"
+                    alt=""
                 />
-                <li style={{ width: "15rem" }}>{img.desc}</li>
-              </ul>
+            ),
+            params: {
+                header: (imgDetail.url
+                    .split('/')
+                    .pop() as string).toUpperCase(),
+                width: '30rem',
+                height: '100%',
+            },
+        });
+    };
+
+    return (
+        <>
+            <h1>
+                {props.name} - {props.info.time}
+            </h1>
+            <section>{props.info.desc}</section>
+            {Object.keys(props.info['modal_details']).map((section) => {
+                return (
+                    <ul>
+                        <li>
+                            {section}
+                            <ul>
+                                {props.info['modal_details'][section].map(
+                                    (point: string) => (
+                                        <li
+                                            dangerouslySetInnerHTML={{
+                                                __html: point,
+                                            }}
+                                        />
+                                    )
+                                )}
+                            </ul>
+                        </li>
+                    </ul>
+                );
+            })}
+            <section>{props.info['images'] ? 'Relevant Media' : ''}</section>
+            <div className="img-grid">
+                {props.info['images']?.map(
+                    (img: { url: string; desc: string }) => {
+                        return (
+                            <div>
+                                <ul>
+                                    <img
+                                        src={process.env.PUBLIC_URL + img.url}
+                                        alt=""
+                                        className="img-btn"
+                                        onClick={() => {
+                                            openImage(img);
+                                        }}
+                                    />
+                                    <li style={{ width: '15rem' }}>
+                                        {img.desc}
+                                    </li>
+                                </ul>
+                            </div>
+                        );
+                    }
+                )}
             </div>
-          );
-        })}
-      </div>
-    </>
-  );
+        </>
+    );
 }
--- src/components/MainOS/Pages/Pages.tsx
+++ src/components/MainOS/Pages/Pages.tsx
@@ -1,135 +1,143 @@
-import React from "react";
-import json from "../../../public/strings";
-import "./Pages.scss";
-import ExperiencePage from "./ExperiencePage";
-import Skills from "./Skills";
-import { $openWindow } from "./../../Parent/ParentService";
+import React from 'react';
+import json from '../../../public/strings';
+import './Pages.scss';
+import ExperiencePage from './ExperiencePage';
+import Skills from './Skills';
+import { $openWindow } from './../../Parent/ParentService';
 
 export default function DesktopFiles(props: any) {
-  let openWindow = (key: string) => {
-    switch (key) {
-      case "Me":
-        return $openWindow.next({
-          child: <Me></Me>,
-          params: {
-            header: "JEFFERSON_LI_INTRO.TXT",
-            width: "38rem",
-            height: "18rem",
-          },
-        });
-      case "Exp":
-        return $openWindow.next({
-          child: (
-            <ExperiencePage openWindow={props.openWindow}></ExperiencePage>
-          ),
-          params: {
-            header: "EXPERIENCES.HTML",
-            width: "38rem",
-            height: "18rem",
-          },
-        });
-      case "Rand":
-        return $openWindow.next({
-          child: <Rand></Rand>,
-          params: {
-            header: "MILK.GIF",
-            width: "30rem",
-            height: "20rem",
-          },
-        });
-      case "Skills":
-        return $openWindow.next({
-          child: <Skills></Skills>,
-          params: {
-            header: "SKILLS.HTML",
-            width: "50rem",
-            height: "40rem",
-          },
-        });
-      case "Resume":
-        return $openWindow.next({
-          child: <Resume></Resume>,
-          params: {
-            header: "RESUME.PDF",
-            width: "38rem",
-            height: "30rem",
-          },
-        });
-      case "Socials":
-        return $openWindow.next({
-          child: <Socials></Socials>,
-          params: {
-            header: "SOCIALS.HTML",
-            width: "25rem",
-            height: "25rem",
-          },
-        });
-      default:
-        return "foo";
-    }
-  };
+    let openWindow = (key: string) => {
+        switch (key) {
+            case 'Me':
+                return $openWindow.next({
+                    child: <Me></Me>,
+                    params: {
+                        header: 'JEFFERSON_LI_INTRO.TXT',
+                        width: '38rem',
+                        height: '18rem',
+                    },
+                });
+            case 'Exp':
+                return $openWindow.next({
+                    child: (
+                        <ExperiencePage
+                            openWindow={props.openWindow}
+                        ></ExperiencePage>
+                    ),
+                    params: {
+                        header: 'EXPERIENCES.HTML',
+                        width: '38rem',
+                        height: '18rem',
+                    },
+                });
+            case 'Rand':
+                return $openWindow.next({
+                    child: <Rand></Rand>,
+                    params: {
+                        header: 'MILK.GIF',
+                        width: '30rem',
+                        height: '20rem',
+                    },
+                });
+            case 'Skills':
+                return $openWindow.next({
+                    child: <Skills></Skills>,
+                    params: {
+                        header: 'SKILLS.HTML',
+                        width: '50rem',
+                        height: '40rem',
+                    },
+                });
+            case 'Resume':
+                return $openWindow.next({
+                    child: <Resume></Resume>,
+                    params: {
+                        header: 'RESUME.PDF',
+                        width: '38rem',
+                        height: '30rem',
+                    },
+                });
+            case 'Socials':
+                return $openWindow.next({
+                    child: <Socials></Socials>,
+                    params: {
+                        header: 'SOCIALS.HTML',
+                        width: '25rem',
+                        height: '25rem',
+                    },
+                });
+            default:
+                return 'foo';
+        }
+    };
 
-  return (
-    <div className="desktop-files">
-      <div className="file" id="me" onClick={() => openWindow("Me")}>
-        JEFFERSON_LI<br></br>_INTRO.TXT
-      </div>
-      <div className="file" id="exp" onClick={() => openWindow("Exp")}>
-        EXPERIENCES<br></br>.HTML
-      </div>
-      <div className="file" id="rand" onClick={() => openWindow("Rand")}>
-        MILK.GIF
-      </div>
-      <div className="file" id="skl" onClick={() => openWindow("Skills")}>
+    return (
+        <div className="desktop-files">
+            <div className="file" id="me" onClick={() => openWindow('Me')}>
+                JEFFERSON_LI<br></br>_INTRO.TXT
+            </div>
+            <div className="file" id="exp" onClick={() => openWindow('Exp')}>
+                EXPERIENCES<br></br>.HTML
+            </div>
+            <div className="file" id="rand" onClick={() => openWindow('Rand')}>
+                MILK.GIF
+            </div>
+            {/* <div className="file" id="skl" onClick={() => openWindow("Skills")}>
         SKILLS.HTML
-      </div>
-      <div className="file" id="res" onClick={() => openWindow("Resume")}>
-        RESUME.PDF
-      </div>
-      <div className="file" id="creds" onClick={() => openWindow("Socials")}>
-        SOCIALS.HTML
-      </div>
-    </div>
-  );
+      </div> */}
+            <div className="file" id="res" onClick={() => openWindow('Resume')}>
+                RESUME.PDF
+            </div>
+            <div
+                className="file"
+                id="creds"
+                onClick={() => openWindow('Socials')}
+            >
+                SOCIALS.HTML
+            </div>
+        </div>
+    );
 }
 
 function Me() {
-  return <div dangerouslySetInnerHTML={{ __html: json.paragraph.join("") }} />;
+    return (
+        <div dangerouslySetInnerHTML={{ __html: json.paragraph.join('') }} />
+    );
 }
 function Rand() {
-  return <img src="gifs/funny_cow.gif" id="rand-gif" alt="funny cow gif" />;
+    return <img src="gifs/funny_cow.gif" id="rand-gif" alt="funny cow gif" />;
 }
 function Resume() {
-  return (
-    <>
-      <embed
-        src="pdfs/resume.pdf"
-        type="application/pdf"
-        width="100%"
-        height="800"
-      ></embed>
-    </>
-  );
+    return (
+        <>
+            <embed
+                src="pdfs/resume.pdf"
+                type="application/pdf"
+                width="100%"
+                height="800"
+            ></embed>
+        </>
+    );
 }
 function Socials() {
-  /* eslint-disable react/jsx-no-target-blank */
-  return (
-    <>
-      {json.socials.map((soc) => {
-        return (
-          <>
-            {soc.name}
-            <ul>
-              <li>
-                <a href={soc.link} target="_blank">
-                  <b>{soc.desc}</b>
-                </a>
-              </li>
-            </ul>
-          </>
-        );
-      })}
-      <div dangerouslySetInnerHTML={{ __html: json.credits.join("") }} />
-    </>
-  );
+    /* eslint-disable react/jsx-no-target-blank */
+    return (
+        <>
+            {json.socials.map((soc) => {
+                return (
+                    <>
+                        {soc.name}
+                        <ul>
+                            <li>
+                                <a href={soc.link} target="_blank">
+                                    <b>{soc.desc}</b>
+                                </a>
+                            </li>
+                        </ul>
+                    </>
+                );
+            })}
+            <div dangerouslySetInnerHTML={{ __html: json.credits.join('') }} />
+        </>
+    );
 }
