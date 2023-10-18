6bba9f590102cedef4c7d94908ecf8e1fcfdaab5
Jefferson(Zhe Fan) Li <jeffersonli2013@gmail.com>
2023-10-18 11:58:50 -0400

    remove mobile banner

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,23 @@ export default class App extends React.Component<any, any> {
   constructor(props: any) {
     super(props);
 
-    let replacementComp = (a: boolean) =>
-      a ? <div id="warning"> Best Experienced on Desktop !</div> : <></>;
-    this.state = {
-      mobile: replacementComp(window.matchMedia("(max-width: 800px)").matches),
-    };
-    let handleEvent = (e: any) => {
-      this.setState({ ...App, mobile: replacementComp(e.matches) });
-    };
-    window
-      .matchMedia("(max-width: 800px)")
-      .addEventListener("change", handleEvent);
+    // let replacementComp = (a: boolean) =>
+    //   a ? <div id="warning"> Best Experienced on Desktop !</div> : <></>;
+    // this.state = {
+    //   mobile: replacementComp(window.matchMedia("(max-width: 800px)").matches),
+    // };
+    // let handleEvent = (e: any) => {
+    //   this.setState({ ...App, mobile: replacementComp(e.matches) });
+    // };
+    // window
+    //   .matchMedia("(max-width: 800px)")
+    //   .addEventListener("change", handleEvent);
   }
 
   render() {
     return (
       <div className="App">
-        {this.state.mobile}
+        {/* {this.state.mobile} */}
         <Parent></Parent>
       </div>
     );
